81deff09a9e4a6b61459e8328aec9bda2e9586ce
SaitamaX <xbw19960123@163.com>
2019-05-03 17:59:20 +0800

    Mnist training finished

--- venv/LeNet5MnistTrain.py
+++ venv/LeNet5MnistTrain.py
@@ -7,7 +7,7 @@ def conv_2d(x, weight, padding):
      # Must have strides[0] = strides[1] = 1, [1, stride, stride, 1]
      # padding:"SAME" or "VALID".
      # Whether to fill the boundary when the image does not match the convolution kernel
-     return tf.nn.conv2d(x, weight, strides= [1, 1, 1, 1],padding=padding)
+     return tf.nn.conv2d(x, weight, strides=[1, 1, 1, 1], padding=padding)
 
 def max_pool_2x2(x, padding):
      # ksize:the size of pooling window [batch, height, width, channels]
@@ -63,10 +63,50 @@ y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-correct_prediction = tf.equal(tf.arg_max(y_conv, 1), tf.arg_max(y_, 1))
+# correct_prediction = tf.equal(tf.arg_max(y_conv, 1), tf.arg_max(y_, 1))
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # accuracy figure
+figure_x = []
+figure_y = []
+plt.figure()
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+
+saver = tf.train.Saver()
+
+epoch_count = 80000
+# feed_dict: The role of feed_dict is to assign values to tensors created using placeholders.
+for i in range(epoch_count):
+     batch = mnist.train.next_batch(50)
+     # print(batch[0])
+     # print(batch[1])
+     if i % 100 == 0:
+          train_accuracy = accuracy.eval(feed_dict = {input_x: batch[0], y_: batch[1]})
+          print("step %d, training accuracy %g" %(i, train_accuracy))
+          figure_x.append(i)
+          figure_y.append(train_accuracy)
+          if i % 1000 == 0:
+               plt.plot(figure_x, figure_y, "b--", linewidth=1)
+               plt.axis([0, epoch_count, 0, 1])
+               plt.show()
+     train_step.run(feed_dict = {input_x: batch[0], y_: batch[1]})
+plt.plot(figure_x, figure_y, "b--", linewidth=1)
+plt.axis([0, epoch_count, 0, 1])
+plt.show()
+
+# saving model
+print("saving model")
+saver.save(sess, "./checkpoint_dir/mnist_model{0}".format(epoch_count))
+print("saving finished")
+
+print("test accuracy %g"%(accuracy.eval(feed_dict = {input_x: mnist.test.images, y_ : mnist.test.labels})))
+print("training finished")
+
 
 
 
